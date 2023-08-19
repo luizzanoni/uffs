5acea548d96501fa54ac7fdaed63ebbe8f767e2d
Luiz Gustavo Zanoni <luizzanoni47@gmail.com>
2023-08-19 19:36:26 -0300

    new hello java

--- prog I/hello.java
+++ prog I/hello.java
@@ -1,5 +1,11 @@
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+// public class hello {
+//     public static void main(String[] args) {
+//         System.out.println("Hello World!");
+//     }
+// } 
+
+class Hello {
+    public static void main(String args[]){
+        System.out.println("Hello world!!");
     }
-} 
+}

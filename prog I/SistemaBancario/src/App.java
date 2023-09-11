4361295d282ddb6ebe285bc677a7f766aba81405
Luiz Gustavo Zanoni <luizzanoni47@gmail.com>
2023-09-11 19:50:17 -0300

    app java

--- prog I/SistemaBancario/src/App.java
+++ prog I/SistemaBancario/src/App.java
@@ -8,26 +8,40 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.print("Digite seu Id: ");
+            System.out.print("Crie seu Id: ");
             int idPessoa = scanner.nextInt();
 
             scanner.nextLine();
 
             System.out.print("Digite seu nome: ");
-            String nomePessoa = scanner.nextLine();
+            String razaoSocial = scanner.nextLine();
 
-            pessoa1.Id = idPessoa;
-            pessoa1.RazaoSocial = nomePessoa;
+            pessoa1.id = idPessoa;
+            pessoa1.razaoSocial = razaoSocial;
 
-            System.out.println(pessoa1.Id + " - " + pessoa1.RazaoSocial);
+            System.out.println("Seu ID e Usuário é:  " + pessoa1.id + " - " + pessoa1.razaoSocial);
 
             System.out.println("Digite 1 para obter um cartão ou 0 para sair: ");
             int opcao = scanner.nextInt();
 
             if (opcao == 0) {
-                System.out.println("Obrigado por escolher nossos serviços " + pessoa1.RazaoSocial);
+                System.out.println("Obrigado por escolher nossos serviços " + pessoa1.razaoSocial);
                 break;
             } else {
+                Agencia agencia1 = new Agencia();
+                System.out.print("Informe o Id da Agência: ");
+                int id = scanner.nextInt();
+
+                scanner.nextLine();
+
+                System.out.print("Digite o nome da Agência: ");
+                String razaoSocialAgencia = scanner.nextLine();
+
+                agencia1.id = id;
+                agencia1.razaoSocialAgencia = razaoSocialAgencia;
+                System.out.println("Sua Agência é: " + agencia1.id + " - " + agencia1.razaoSocialAgencia);
+
+
                 CartaoCredito cartao1 = new CartaoCredito();
                 System.out.print("Digite o número de seu cartão: ");
                 int numero = scanner.nextInt();
@@ -37,9 +51,26 @@ public class App {
                 System.out.print("Digite a data de validade: ");
                 String dataValidade = scanner.nextLine();
 
-                cartao1.Numero = numero;
-                cartao1.DataValidade = dataValidade;
-                System.out.println("O numero do seu cartão é: " + cartao1.Numero + " e ele vence dia: " + cartao1.DataValidade);
+                cartao1.numero = numero;
+                cartao1.dataValidade = dataValidade;
+                System.out.println("O numero do seu cartão é: " + cartao1.numero + " e ele vence dia: " + cartao1.dataValidade);
+
+                System.out.println("Digite 1 para editar a Agência ou 0 para sair: ");
+                int opcao1 = scanner.nextInt();
+
+                if (opcao1 == 1) {
+                    int novoIdAg = scanner.nextInt();
+                    agencia1.setId(novoIdAg);
+                    String novoNomeAg = scanner.nextLine();
+                    agencia1.setRazaoSocialAgencia(novoNomeAg);
+
+                    System.out.print(agencia1.id + " - " + agencia1.razaoSocialAgencia);
+                    scanner.nextLine();
+                } else {
+                    System.out.println("Obrigado por escolher nossos serviços " + pessoa1.razaoSocial);
+                    break;
+                }
+
             }
         }
         scanner.close();

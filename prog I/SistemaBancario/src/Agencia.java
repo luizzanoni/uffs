c30dd32f77d5497c93149b687c456e39be6cefde
Luiz Gustavo Zanoni <luizzanoni47@gmail.com>
2023-09-11 19:49:56 -0300

    agencia

--- prog I/SistemaBancario/src/Agencia.java
+++ prog I/SistemaBancario/src/Agencia.java
@@ -1,3 +1,23 @@
 public class Agencia {
     
+    int id;
+
+    String razaoSocialAgencia;
+
+    public int getId() {
+        return this.id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRazaoSocialAgencia() {
+        return this.razaoSocialAgencia;
+    }
+    
+    public void setRazaoSocialAgencia(String razaoSocialAgencia) {
+        this.razaoSocialAgencia = razaoSocialAgencia;
+    }
 }
+

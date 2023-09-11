41cf60b6cf0eb599f9e7f1192f07cdb95bc3051f
Luiz Gustavo Zanoni <luizzanoni47@gmail.com>
2023-09-11 20:38:17 -0300

    upp

--- prog I/SistemaBancario/src/CartaoCredito.java
+++ prog I/SistemaBancario/src/CartaoCredito.java
@@ -1,6 +1,24 @@
 public class CartaoCredito {
     
-    int Numero;
+    private int numero;
 
-    String DataValidade;
+    public String dataValidade;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    
 }
